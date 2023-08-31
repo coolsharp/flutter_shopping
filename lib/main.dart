d2d60b6ca05c40e330d7c223f3b2c4ec8c4c510f
coolsharp <coolsharp@gmail.com>
2023-08-31 17:25:29 +0900

    feat: 상품상세 페이지 전환 이벤트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shopping/product.dart';
+import 'package:shopping/productDetailPage.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-List? categeryList = null;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +21,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      initialRoute: '/',
+      routes: {'/': (context) => MainPage(),
+      '/ProductDetailPage': (context) => ProductDetailPage()},
     );
   }
 }
@@ -39,12 +42,21 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text('Shopping'),
       ),
-      body: ViewPagerExample(),
+      body: ShoppingList(),
     );
   }
 }
 
-class TwoColumnGrid extends StatelessWidget {
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({super.key});
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  List<Product> products = [];
+
   @override
   Widget build(BuildContext context) {
     // 아이템 목록 생성
@@ -54,89 +66,130 @@ class TwoColumnGrid extends StatelessWidget {
       // 열의 개수를 지정합니다.
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
+        childAspectRatio: 0.7,
       ),
       // 아이템 생성
       itemBuilder: (context, index) {
-        return Card(
-          child: Center(
-            child: Text(items[index]),
+        final product = products[index];
+        double screenWidth = MediaQuery.of(context).size.width;
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed('/ProductDetailPage');
+          },
+          child: Card(
+            semanticContainer: true,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(7.0), // 상단 왼쪽 모서리
+                    topRight: Radius.circular(7.0), // 상단 오른쪽 모서리
+                  ),
+                  child: Image.network(
+                    product.images[0],
+                    height: screenWidth / 2 / 1.05,
+                    width: screenWidth / 2,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 4, left: 8, right: 8, bottom: 0),
+                  child: Text(
+                    product.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 0, left: 8, right: 8, bottom: 1),
+                  child: Text(
+                    product.brand,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 2, left: 8, right: 8, bottom: 1),
+                  child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                          children: [
+                            const TextSpan(
+                              text: '\$', // Small dollar sign
+                              style: TextStyle(
+                                fontSize: 14.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '${product.price}', // Larger amount
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        '${product.discountPercentage} OFF',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(fontSize: 16, color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
       // 아이템 개수
-      itemCount: items.length,
-    );
-  }
-}
-
-class ViewPagerExample extends StatefulWidget {
-  const ViewPagerExample({super.key});
-
-  @override
-  State<ViewPagerExample> createState() => _ViewPagerExampleState();
-}
-
-class _ViewPagerExampleState extends State<ViewPagerExample> with SingleTickerProviderStateMixin {
-  TabController? _tabController;
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: categeryList!.length, // 탭의 수
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('TabBar & PageView 예제'),
-          bottom: TabBar(
-            controller: _tabController,
-            isScrollable: true,
-            tabs: [
-              for (int i = 0; i < categeryList!.length; i++) Tab(text: '${categeryList![i]}'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            for (int i = 0; i < categeryList!.length; i++)
-              Center(
-                child: TwoColumnGrid(),
-              ),
-          ],
-        ),
-      ),
+      itemCount: products.length,
     );
   }
 
   @override
   void initState() {
     super.initState();
-    getJsonData();
+    fetchData();
   }
 
-  Future<String> getJsonData() async {
-    var url = 'https://dummyjson.com/products/categories';
-    var response = await http.get(Uri.parse(url));
-    var dataConvertedToJson = jsonDecode(response.body);
-    setState(() {
-      categeryList = dataConvertedToJson;
-      _tabController = TabController(vsync: this, length: categeryList!.length);
-    });
-    _tabController!.addListener(() {
-      if (_tabController!.indexIsChanging) {
-        print('Change index ${categeryList![_tabController!.index]}');
-      } else if(_tabController!.index != _tabController!.previousIndex) {
-        print('Different index ${categeryList![_tabController!.index]}');
-      }
-    });
-    for (int i = 0; i < categeryList!.length; i++) {
-      print(categeryList![i]);
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://dummyjson.com/products'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> productsData = data['products'];
+
+      final List<Product> loadedProducts = productsData.map((productData) {
+        return Product.fromJson(productData);
+      }).toList();
+
+      setState(() {
+        products = loadedProducts;
+      });
+    } else {
+      throw Exception('Failed to load data');
     }
-    return 'Successfull';
   }
 
   @override
-  void dispose() {
-    _tabController!.dispose();
-    super.dispose();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
   }
 }
