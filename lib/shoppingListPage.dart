e71cc33de7fccc8bb0193f53f720363fe610e550
coolsharp <coolsharp@gmail.com>
2023-09-05 17:16:18 +0900

    feat: 상품이 없을 경우 예외처리

--- lib/shoppingListPage.dart
+++ lib/shoppingListPage.dart
@@ -7,7 +7,8 @@ import 'package:shopping/product.dart';
 import 'package:shopping/productDetailPage.dart';
 
 class ShoppingList extends StatefulWidget {
-  const ShoppingList({super.key, required  this.category, required  this.keyword});
+  const ShoppingList(
+      {super.key, required this.category, required this.keyword});
 
   final category;
 
@@ -48,136 +49,142 @@ class _ShoppingListState extends State<ShoppingList> {
 
     return _isFirstLoadRunning // 최초 로딩일 경우 로딩 프로그래스 표시
         ? const Center(
-            child: const CircularProgressIndicator(),
-          )
-        : Column(
-            children: [
-              Expanded(
-                child: GridView.builder(
-                  // 최초 로딩이 아닐 경우 리스트 표시
-                  // 열의 개수를 지정합니다.
-                  controller: _controller,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.7,
-                  ),
-                  // 아이템 생성
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    double screenWidth = MediaQuery.of(context).size.width;
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  ProductDetailPage(product.id),
-                            ));
-                      },
-                      child: Card(
-                        semanticContainer: true,
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      child: const CircularProgressIndicator(),
+    )
+        : _products.isNotEmpty
+        ? Column(
+      children: [
+        Expanded(
+          child: GridView.builder(
+            // 최초 로딩이 아닐 경우 리스트 표시
+            // 열의 개수를 지정합니다.
+            controller: _controller,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.7,
+            ),
+            // 아이템 생성
+            itemBuilder: (context, index) {
+              final product = _products[index];
+              double screenWidth = MediaQuery
+                  .of(context)
+                  .size
+                  .width;
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ProductDetailPage(product.id),
+                      ));
+                },
+                child: Card(
+                  semanticContainer: true,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(7.0), // 상단 왼쪽 모서리
+                          topRight: Radius.circular(7.0), // 상단 오른쪽 모서리
+                        ),
+                        child: Image.network(
+                          product.images[0],
+                          height: screenWidth / 2 / 1.05,
+                          width: screenWidth / 2,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 4, left: 8, right: 8, bottom: 0),
+                        child: Text(
+                          product.title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 0, left: 8, right: 8, bottom: 1),
+                        child: Text(
+                          product.brand,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 2, left: 8, right: 8, bottom: 1),
+                        child: Row(
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceBetween,
                           children: [
-                            ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(7.0), // 상단 왼쪽 모서리
-                                topRight: Radius.circular(7.0), // 상단 오른쪽 모서리
-                              ),
-                              child: Image.network(
-                                product.images[0],
-                                height: screenWidth / 2 / 1.05,
-                                width: screenWidth / 2,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 4, left: 8, right: 8, bottom: 0),
-                              child: Text(
-                                product.title,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
+                            RichText(
+                              text: TextSpan(
                                 style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 0, left: 8, right: 8, bottom: 1),
-                              child: Text(
-                                product.brand,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 2, left: 8, right: 8, bottom: 1),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                  fontSize: 20.0,
+                                  color: Colors.black,
+                                ),
                                 children: [
-                                  RichText(
-                                    text: TextSpan(
-                                      style: const TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.black,
-                                      ),
-                                      children: [
-                                        const TextSpan(
-                                          text: '\$', // Small dollar sign
-                                          style: TextStyle(
-                                            fontSize: 14.0,
-                                          ),
-                                        ),
-                                        TextSpan(
-                                          text:
-                                              '${product.price}', // Larger amount
-                                        ),
-                                      ],
+                                  const TextSpan(
+                                    text: '\$', // Small dollar sign
+                                    style: TextStyle(
+                                      fontSize: 14.0,
                                     ),
                                   ),
-                                  Text(
-                                    '${product.discountPercentage} OFF',
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: const TextStyle(
-                                        fontSize: 16, color: Colors.red),
+                                  TextSpan(
+                                    text:
+                                    '${product.price}', // Larger amount
                                   ),
                                 ],
                               ),
                             ),
+                            Text(
+                              '${product.discountPercentage} OFF',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.red),
+                            ),
                           ],
                         ),
                       ),
-                    );
-                  },
-                  // 아이템 개수
-                  itemCount: _products.length,
-                ),
-              ),
-              // when the _loadMore function is running
-              if (_isLoadMoreRunning == true)
-                const Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 40),
-                  child: Center(
-                    child: CircularProgressIndicator(),
+                    ],
                   ),
                 ),
-            ],
-          );
+              );
+            },
+            // 아이템 개수
+            itemCount: _products.length,
+          ),
+        ),
+        // when the _loadMore function is running
+        if (_isLoadMoreRunning == true)
+          const Padding(
+            padding: EdgeInsets.only(top: 10, bottom: 40),
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
+    ) :
+    Center(child: Text('상품이 없습니다.'),);
   }
 
   @override
   void initState() {
     super.initState();
     _firstLoad();
-    _controller = ScrollController()..addListener(_loadMore);
+    _controller = ScrollController()
+      ..addListener(_loadMore);
   }
 
   // 앱이 실행되면 호출되는 함수
@@ -187,12 +194,15 @@ class _ShoppingListState extends State<ShoppingList> {
     });
     try {
       String url =
-          '${_baseUrl}products/category/${widget.category}?limit=$_limit&skip=$_skip';
+          '${_baseUrl}products/category/${widget
+          .category}?limit=$_limit&skip=$_skip';
       if ('all' == widget.category) {
         url = '${_baseUrl}products?limit=$_limit&skip=$_skip';
       }
       if (null != widget.keyword && widget.keyword.isNotEmpty) {
-        url = '${_baseUrl}products/search?q=${widget.keyword}&limit=$_limit&skip=$_skip';
+        url =
+        '${_baseUrl}products/search?q=${widget
+            .keyword}&limit=$_limit&skip=$_skip';
       }
 
       final response = await http.get(Uri.parse(url));
@@ -229,16 +239,18 @@ class _ShoppingListState extends State<ShoppingList> {
       _skip += 20; // Increase _page by 1
       try {
         String url =
-            '${_baseUrl}products/category/${widget.category}?limit=$_limit&skip=$_skip';
+            '${_baseUrl}products/category/${widget
+            .category}?limit=$_limit&skip=$_skip';
         if ('all' == widget.category) {
           url = '${_baseUrl}products?limit=$_limit&skip=$_skip';
         }
         if (null != widget.keyword && widget.keyword.isNotEmpty) {
-          url = '${_baseUrl}products/search?q=${widget.keyword}&limit=$_limit&skip=$_skip';
+          url =
+          '${_baseUrl}products/search?q=${widget
+              .keyword}&limit=$_limit&skip=$_skip';
         }
 
-        final response = await http
-            .get(Uri.parse(url));
+        final response = await http.get(Uri.parse(url));
         if (response.statusCode == 200) {
           final Map<String, dynamic> data = json.decode(response.body);
           final List<dynamic> productsData = data['products'];
